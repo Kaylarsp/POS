d4928880bb1267537083f46e274856e1e04c33d7
Kayla <satitiputri.kayla@gmail.com>
2025-03-10 13:27:42 +0700

    Jobsheet 5, Praktikum 1: Integrasi Laravel dengan AdminLte3

--- routes/web.php
+++ routes/web.php
@@ -12,20 +12,6 @@ use Monolog\Handler\RotatingFileHandler;
 // Halaman Home
 Route::get('/', [HomeController::class, 'home']);
 
-// Halaman Products dengan Prefix "/category"
-Route::prefix('category')->group(function () {
-    Route::get('/food-beverage', [ProductsController::class, 'foodBeverage'])->name('category.food-beverage');
-    Route::get('/beauty-health', [ProductsController::class, 'beautyHealth'])->name('category.beauty-health');
-    Route::get('/home-care', [ProductsController::class, 'homeCare'])->name('category.home-care');
-    Route::get('/baby-kid', [ProductsController::class, 'babyKid'])->name('category.baby-kid');
-});
-
-// Halaman User profile
-Route::get('/profile', [UserController::class, 'profile'])->name('profile');
-
-// Halaman Penjualan
-Route::get('/sales', [SalesController::class, 'index'])->name('sales');
-
 Route::get('/level', [LevelController::class, 'index']);
 Route::get('/kategori', [KategoriController::class, 'index']);
 Route::get('/user', [UserController::class, 'index']);
