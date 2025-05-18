559495dc850e108a531ad76446401fcc8997f34b
Kayla <satitiputri.kayla@gmail.com>
2025-05-18 16:54:25 +0700

    Jobsheet 10, Praktikum 2:  Membuat RESTful API Login

--- routes/web.php
+++ routes/web.php
@@ -11,8 +11,8 @@ use App\Http\Controllers\WelcomeController;
 
 Route::pattern('id', '[0-9]+');
 
-Route::get('login', [AuthController::class, 'login'])->name('login');
-Route::post('login', [AuthController::class, 'postlogin']);
+// Route::get('login', [AuthController::class, 'login'])->name('login');
+// Route::post('login', [AuthController::class, 'postlogin']);
 Route::get('logout', [AuthController::class, 'logout'])->middleware('auth');
 
 // Route::get('register', [AuthController::class, 'register'])->name('register');
