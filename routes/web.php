eb5c2e671a0c653c183f3458a06f237604568158
Kayla <satitiputri.kayla@gmail.com>
2025-05-18 16:05:44 +0700

    Jobsheet 10: Membuat RESTful API Register

--- routes/web.php
+++ routes/web.php
@@ -15,8 +15,8 @@ Route::get('login', [AuthController::class, 'login'])->name('login');
 Route::post('login', [AuthController::class, 'postlogin']);
 Route::get('logout', [AuthController::class, 'logout'])->middleware('auth');
 
-Route::get('register', [AuthController::class, 'register'])->name('register');
-Route::post('register', [AuthController::class, 'postRegister']);
+// Route::get('register', [AuthController::class, 'register'])->name('register');
+// Route::post('register', [AuthController::class, 'postRegister']);
 
 Route::middleware(['auth'])->group(function () {
 
